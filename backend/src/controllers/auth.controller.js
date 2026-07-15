9030eacfdcde5208c59f9faf13f8da0da22fcbf5
Nitin Kumar <nitinkumar29122501@gmail.com>
2026-07-16 00:38:52 +0530

    updateProfile

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -102,5 +103,24 @@ export const logout = (_, res) => {
 };
 
 export const updateProfile = async (req,res) => {
+try {
+    const {profilePic} = req.body;
+    if(!profilePic) return res.status(400).json({ message: "Profile pic is required"})
 
+     const userId = req.user._id;   
+
+     const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+     const updateUser =  await User.findByIdAndUpdate(
+        userId,
+        {profilePic: uploadResponse.secure_url},
+        {new: true}
+     );
+
+     res.status(200).json(updateUser);
+} catch (error) {
+    console.log("Error in updating profile" , error);
+    res.status(500).json({message: "Internal server error"});
+    
+}
 }
--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import { process } from "zod/v4/core";
 
 export const ENV = {
     PORT: process.env.PORT,
@@ -9,4 +10,10 @@ export const ENV = {
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
     EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    
+
+
 };
