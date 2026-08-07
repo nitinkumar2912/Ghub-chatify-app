844ed5b26c5c5f588232b577ea44b51756a26ab7
Nitin Kumar <nitinkumar29122501@gmail.com>
2026-08-07 23:52:35 +0530

    getMessageByUserId controller

--- backend/src/controllers/message.controler.js
+++ backend/src/controllers/message.controler.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 
@@ -10,6 +11,53 @@ export const getAllContacts = async (req , res) => {
     } catch (error) {
         console.log("Error in getAllContacts:",error);
         res.status(500).json({message: "Server error"});
+        
+    }
+}
+
+export const getMessageByUserId = async (req,res) => {
+    try {
+        const myId = req.user._id;
+        const {id: usertoChatId} = req.params;
+
+        const message = await Message.find(
+            {
+                $or: [
+                    {senderId: myId, receiverId: usertoChatId},
+                    {senderId: usertoChatId, receiverId: myId}
+                ]
+            }
+        );
+        res.status(200).json(messagec)
+    } catch (error) {
+        console.log("Error in getMessages controller :",error.message);
+        res.status(500).json({message: "Internal Server error"});
+    }
+}
+
+export const sendMessage = async (req,res) => {
+    try {
+        const [text ,image] = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageurl;
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message ({
+            senderId,
+            receiverId,
+            text,
+            image: imageurl,
+        });
+
+        await newMessage.save()
+
+        res.status(201).json(newMessage);
+    } catch (error) {
         
     }
 }
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,15 +1,15 @@
 
 import express from "express";
-import { getAllContacts } from "../controllers/message.controler.js";
+import { getAllContacts ,getMessageByUserId , sendMessage } from "../controllers/message.controler.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/contacts",protectRoute,getAllContacts);
 // router.get("/chats",getChatPartners);
-// router.get(":id",getMessageByUserId)
+router.get(":id",protectRoute,getMessageByUserId)
 
 
-// router.post("/send/:id" , sendMessage);   
+router.post("/send/:id" ,protectRoute, sendMessage);   
 
 export default router;
