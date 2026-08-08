629e6d48097deae5bef29a9d821eb8c4ccb2d759
Nitin Kumar <nitinkumar29122501@gmail.com>
2026-08-08 23:19:58 +0530

    sendMessage controller

--- backend/src/controllers/message.controler.js
+++ backend/src/controllers/message.controler.js
@@ -37,11 +37,11 @@ export const getMessageByUserId = async (req,res) => {
 
 export const sendMessage = async (req,res) => {
     try {
-        const [text ,image] = req.body;
+        const {text ,image} = req.body;
         const {id: receiverId} = req.params;
         const senderId = req.user._id;
 
-        let imageurl;
+        let imageUrl;
         if(image) {
             const uploadResponse = await cloudinary.uploader.upload(image);
             imageUrl = uploadResponse.secure_url;
@@ -51,7 +51,7 @@ export const sendMessage = async (req,res) => {
             senderId,
             receiverId,
             text,
-            image: imageurl,
+            image: imageUrl,
         });
 
         await newMessage.save()
